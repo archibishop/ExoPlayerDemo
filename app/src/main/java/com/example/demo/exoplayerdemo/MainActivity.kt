87615a6f2282b2a0f6872ecf9a9f728b204813a0
Wagubi Brian <wagubib@gmail.com>
2019-05-15 15:58:44 +0300

    Add progress to state listener.

--- app/src/main/java/com/example/demo/exoplayerdemo/MainActivity.kt
+++ app/src/main/java/com/example/demo/exoplayerdemo/MainActivity.kt
@@ -2,35 +2,48 @@ package com.example.demo.exoplayerdemo
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.Surface
 import android.view.View
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.ExoPlayerFactory
+import android.widget.ProgressBar
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.audio.AudioRendererEventListener
+import com.google.android.exoplayer2.decoder.DecoderCounters
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import com.google.android.exoplayer2.video.VideoRendererEventListener
+import com.google.android.exoplayer2.ExoPlayer
+
+
 
 
 class MainActivity : Activity() {
 
     private lateinit var playerView: PlayerView
+    private lateinit var componentListener: ComponentListener
     private var player: ExoPlayer? = null
 
     private var playWhenReady = true
     private var currentWindow = 0
     private var playbackPosition: Long = 0
+    private lateinit var progressBar: ProgressBar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         playerView = findViewById(R.id.video_view)
+        progressBar = findViewById(R.id.progress_bar)
+
+        componentListener = ComponentListener()
 
     }
 
@@ -40,6 +53,8 @@ class MainActivity : Activity() {
 
         player?.playWhenReady = playWhenReady
         player?.seekTo(currentWindow, playbackPosition)
+
+        player?.addListener(componentListener)
 
         var uri =  Uri.parse("https://archive.org/download/ElephantsDream/ed_hd.mp4")
 
@@ -97,8 +112,95 @@ class MainActivity : Activity() {
             playbackPosition = it.currentPosition
             currentWindow = it.currentWindowIndex
             playWhenReady = it.playWhenReady
+            it.addListener(componentListener)
             it.release()
             player = null
         }
     }
+
+    inner class ComponentListener: Player.DefaultEventListener(), VideoRendererEventListener, AudioRendererEventListener {
+        override fun onDroppedFrames(count: Int, elapsedMs: Long) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onVideoEnabled(counters: DecoderCounters?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onVideoSizeChanged(
+            width: Int,
+            height: Int,
+            unappliedRotationDegrees: Int,
+            pixelWidthHeightRatio: Float
+        ) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onVideoDisabled(counters: DecoderCounters?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onVideoDecoderInitialized(
+            decoderName: String?,
+            initializedTimestampMs: Long,
+            initializationDurationMs: Long
+        ) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onVideoInputFormatChanged(format: Format?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onRenderedFirstFrame(surface: Surface?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onAudioSinkUnderrun(bufferSize: Int, bufferSizeMs: Long, elapsedSinceLastFeedMs: Long) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onAudioEnabled(counters: DecoderCounters?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onAudioInputFormatChanged(format: Format?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onAudioSessionId(audioSessionId: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onAudioDecoderInitialized(
+            decoderName: String?,
+            initializedTimestampMs: Long,
+            initializationDurationMs: Long
+        ) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onAudioDisabled(counters: DecoderCounters?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            super.onPlayerStateChanged(playWhenReady, playbackState)
+            var stateString: String?
+            when (playbackState) {
+                ExoPlayer.STATE_IDLE -> stateString = "ExoPlayer.STATE_IDLE      -"
+                ExoPlayer.STATE_BUFFERING -> {
+                    stateString = "ExoPlayer.STATE_BUFFERING -"
+                    progressBar.visibility = View.VISIBLE
+                }
+                ExoPlayer.STATE_READY -> {
+                    stateString = "ExoPlayer.STATE_READY     -"
+                    progressBar.visibility = View.GONE
+                }
+                ExoPlayer.STATE_ENDED -> stateString = "ExoPlayer.STATE_ENDED     -"
+                else -> stateString = "UNKNOWN_STATE             -"
+            }
+        }
+    }
+
 }
